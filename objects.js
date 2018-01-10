f0f7713a4d9eb14d08e0e776f744f08c447b818c
Joe Demeo <puredrag0n@yahoo.com>
2018-01-10 23:47:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { eminem: "balls" };
 
 function updatePlaylist (playlist, artist, title) {
-  return Object.assign({}, playlisy, { [artist]: title });
+  return Object.assign({}, playlist, { [artist]: title });
 
 }
